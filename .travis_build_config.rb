b6ce259fa18c88f165a990fa7bf4d2091301884a
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-05-11 22:41:26 -0700

    Add dependecy mrbgems

--- .travis_build_config.rb
+++ .travis_build_config.rb
@@ -4,5 +4,6 @@ MRuby::Build.new do |conf|
   conf.gem '../mruby-http2'
   # link other mrbgem for now; catch segfault bug when rake all test; 2014/05/06
   conf.gem :github => "iij/mruby-io"
-  conf.gem :github => "mattn/mruby-http"
+  conf.gem :github => "iij/mruby-socket"
+  conf.gem :github => "mattn/mruby-simplehttp"
 end
