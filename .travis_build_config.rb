fd21488ec36a31fa7efc5b19e6ba0d43afa515cb
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-05-11 22:43:55 -0700

    Fix typo

--- .travis_build_config.rb
+++ .travis_build_config.rb
@@ -5,5 +5,5 @@ MRuby::Build.new do |conf|
   # link other mrbgem for now; catch segfault bug when rake all test; 2014/05/06
   conf.gem :github => "iij/mruby-io"
   conf.gem :github => "iij/mruby-socket"
-  conf.gem :github => "mattn/mruby-simplehttp"
+  conf.gem :github => "matsumoto-r/mruby-simplehttp"
 end
