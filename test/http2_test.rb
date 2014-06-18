796beb3bc35a07a7236eb6cf27422692ec484d0d
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-06-18 08:56:51 -0700

    Change test site to nghttp2.org

--- test/http2_test.rb
+++ test/http2_test.rb
@@ -1,28 +1,20 @@
-test_site = 'https://http2.matsumoto-r.jp:58080/index.html'
+test_site = 'https://nghttp2.org/'
 assert("HTTP2::Client#request_headers") do
   r = HTTP2::Client.get test_site
   assert_equal("GET", r.request_headers[":method"])
-  assert_equal("/index.html", r.request_headers[":path"])
+  assert_equal("/", r.request_headers[":path"])
   assert_equal("https", r.request_headers[":scheme"])
-  assert_equal("http2.matsumoto-r.jp:58080", r.request_headers[":authority"])
+  assert_equal("nghttp2.org", r.request_headers[":authority"])
   assert_equal("*/*", r.request_headers["accept"])
 end
 assert("HTTP2::Client#status") do
   r = HTTP2::Client.get test_site
   assert_equal(200, r.status)
 end
-assert("HTTP2::Client#body") do
-  r = HTTP2::Client.get test_site
-  assert_equal("<html><body><h1>Your connection with HTTP/2 is success.</h1></body></html>\n", r.body)
-end
-assert("HTTP2::Client#body_length") do
-  r = HTTP2::Client.get test_site
-  assert_equal(75, r.body_length)
-end
+
 assert("HTTP2::Client#uri,get") do
   s = HTTP2::Client.new
   s.uri = test_site
   r = s.get
-  assert_equal("<html><body><h1>Your connection with HTTP/2 is success.</h1></body></html>\n", r.body)
-  assert_equal(75, r.body_length)
+  assert_equal(200, r.status)
 end
