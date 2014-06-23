40a160927d84207e595ae84ebf5e3012f8f06912
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-06-23 05:00:45 -0700

    Update build_config

--- .travis_build_config.rb
+++ .travis_build_config.rb
@@ -1,9 +1,10 @@
 MRuby::Build.new do |conf|
   toolchain :gcc
   conf.gembox 'default'
-  conf.gem '../mruby-http2'
+
   # link other mrbgem for now; catch segfault bug when rake all test; 2014/05/06
   conf.gem :github => "iij/mruby-io"
   conf.gem :github => "iij/mruby-socket"
   conf.gem :github => "matsumoto-r/mruby-simplehttp"
+  conf.gem '../mruby-http2'
 end
