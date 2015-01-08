a987950c0085f9dfb0edefd929c1f0e2caab2beb
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-01-08 00:59:09 -0800

    Add mruby-pack for ci

--- .travis_build_config.rb
+++ .travis_build_config.rb
@@ -5,6 +5,7 @@ MRuby::Build.new do |conf|
   # link other mrbgem for now; catch segfault bug when rake all test; 2014/05/06
   conf.gem :github => "iij/mruby-io"
   conf.gem :github => "iij/mruby-socket"
+  conf.gem :github => "iij/mruby-pack"
   conf.gem :github => "matsumoto-r/mruby-simplehttp"
   conf.gem '../mruby-http2'
 end
