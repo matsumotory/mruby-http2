dcc722790eca8647d013b6635d0301689bc4422c
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-01-09 08:12:51 -0800

    Add server test using nghttp from localhost

--- example/http2_server.rb
+++ example/http2_server.rb
@@ -2,67 +2,12 @@ root_dir = "/usr/local/trusterd"
 
 s = HTTP2::Server.new({
 
-  #
-  # required config
-  #
-
   :port           => 8080,
   :document_root  => "#{root_dir}/htdocs",
   :server_name    => "mruby-http2 server",
 
-  # required when tls option is true.
-  # tls option is true by default.
-
-  :key            => "#{root_dir}/ssl/server.key",
-  :crt            => "#{root_dir}/ssl/server.crt",
-
-  # listen ip address
-  # default value is 0.0.0.0
-  # :server_host  => "127.0.0.1",
-
-  #
-  # optional config
-  #
-
-  # debug default: false
-  # :debug  =>  true,
-
-  # tls default: true
-  # :tls => false,
-
-  # damone default: false
-  # :daemon => true,
-
-  # callback default: false
-  # :callback => true,
-
-  # connection_record defualt: true
-  # :connection_record => false,
-
+  :tls => false,
+  :daemon => true,
 })
-
-#
-# when :callback option is true,
-#
-# s.set_map_to_strage_cb {
-#
-#   p "callback bloack at set_map_to_strage_cb"
-#   p s.request.uri
-#   p s.request.filename
-#
-#   # location setting
-#   if s.request.uri == "/index.html"
-#     s.request.filename = "#{root_dir}/htdocs/hoge"
-#   end
-#   p s.request.filename
-#
-# }
-
-# s.set_logging_cb {
-#
-#   p "callback block after send response"
-#   p "response file: #{s.request.filename}"
-#
-# }
 
 s.run
