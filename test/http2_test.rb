db5ad6f05f8179fa6b1c80fea46267e65e38acff
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-01-09 08:06:19 -0800

    Change test url for test

--- test/http2_test.rb
+++ test/http2_test.rb
@@ -1,11 +1,11 @@
-test_site = 'https://http2.matsumoto-r.jp:58080/index.html'
+test_site = 'https://nghttp2.org/'
 
 assert("HTTP2::Client#request_headers") do
   r = HTTP2::Client.get test_site
   assert_equal("GET", r.request_headers[":method"])
-  assert_equal("/index.html", r.request_headers[":path"])
+  assert_equal("/", r.request_headers[":path"])
   assert_equal("https", r.request_headers[":scheme"])
-  assert_equal("http2.matsumoto-r.jp:58080", r.request_headers[":authority"])
+  assert_equal("nghttp2.org", r.request_headers[":authority"])
   assert_equal("*/*", r.request_headers["accept"])
 end
 
