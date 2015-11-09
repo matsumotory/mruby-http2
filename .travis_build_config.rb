799bde769203fefb019908d0c2fc5c38ab999bba
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-11-09 13:40:33 +0900

    Enable unit test for http2 clinet

--- .travis_build_config.rb
+++ .travis_build_config.rb
@@ -9,3 +9,19 @@ MRuby::Build.new do |conf|
   conf.gem :github => "matsumoto-r/mruby-simplehttp"
   conf.gem '../mruby-http2'
 end
+
+MRuby::Build.new('test') do |conf|
+  # Gets set by the VS command prompts.
+  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
+    toolchain :visualcpp
+  else
+    toolchain :gcc
+  end
+
+  enable_debug
+  conf.enable_bintest
+  conf.enable_test
+
+  conf.gembox 'default'
+end
+
